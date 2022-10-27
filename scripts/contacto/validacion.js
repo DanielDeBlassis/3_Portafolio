9e992e57ea7abc0a904a0b67685b18af0d5ac523
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-10-27 05:21:00 -0300

    Crea arreglo con los mensajes de errores

--- scripts/contacto/validacion.js
+++ scripts/contacto/validacion.js
@@ -34,3 +34,21 @@ const tipoDeErrores = [
     "customError",
 ];
 
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo nombre no puede estar vacío",
+        patternMismatch: "El nombre debe contener entre 1 a 50 caracteres.",
+    },
+    email: {
+        valueMissing: "El campo correo no puede estar vacío",
+        typeMismatch: "El correo no es válido",
+    },
+    asunto: {
+        valueMissing: "El campo asunto no puede estar vacío",
+        patternMismatch: "El asunto debe contener entre 1 a 50 caracteres.",
+    },
+    mensaje: {
+        valueMissing: "El campo mensaje no puede estar vacío",
+        patternMismatch: "El mensaje debe contener entre 1 a 300 caracteres.",
+    }
+};
