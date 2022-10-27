84acc05cb2da5aba7dfa5edf98a299ff859e8768
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-10-27 05:17:59 -0300

    Crea función validar
    La misma recibe un parámetro de tipo input o textarea, obtiene y guarda en una constante el tipo de información que guarda (como el nombre o email), luego obtiene el contenedor del mensaje de error en caso de que hubieran, y chequea que el input sea válido.
    En caso de que el input sea válido, se le remueve la clase error, se resetean los mensajes de error y se oculta el contenedor los mismos.
    En caso de que el input no sea válido, se le añadirá la clase error al mismo, se agregarán los mensajes de error al contenedor (recurriendo a una función para ello), y se removerá la clase que los oculta.

--- scripts/contacto/validacion.js
+++ scripts/contacto/validacion.js
@@ -1,3 +1,18 @@
 //Deshabilitar el botón
 const $boton = document.querySelector("#button");
 $boton.setAttribute("disabled", "disabled");
+
+export function validar(input) {
+    const tipoDeInput = input.dataset.tipo;
+    const $msjError = input.parentElement.querySelector(".input-message-error");
+
+    if (input.validity.valid) {
+        input.classList.remove("error");
+        $msjError.innerHTML = "";
+        $msjError.classList.add("no-mostrar");
+    } else {
+        input.classList.add("error");
+        $msjError.innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+        $msjError.classList.remove("no-mostrar");
+    }
+}
